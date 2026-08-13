c5a163b84d23ed5e248317b36bc3b5a936f65b83
logicforce <sejal.sharma199x@gmail.com>
2026-08-13 15:34:36 +0530

    Time: 20 ms (83.32%) | Memory: 56.6 MB (38.59%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -5,6 +5,10 @@ class Solution {
         int left =  0;
         int right = people.length-1;
         while(left<= right) {
+            if (left == right) {
+            boats++;
+            break;
+    }
             if(people[left]+people[right]<= limit) {
                 left++;
             }
