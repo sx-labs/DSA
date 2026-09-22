0de0f410d9b14481a2574585988bc137c58a2a62
logicforce <sejal.sharma199x@gmail.com>
2026-09-22 12:18:21 +0530

    Time: 7 ms (99.96%) | Memory: 47.7 MB (43.39%) - LeetSync

---
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -0,0 +1,32 @@
+class NumArray {
+
+    int prefix[]; //belongs to the whole obj (it initializes and say i need a prefix array)
+
+    public NumArray(int[] nums) {
+
+        //you create the prefix array here when nums arrive
+        prefix = new int[nums.length];
+        prefix[0] = nums[0];
+        for(int i= 1 ; i<nums.length ; i++) {
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+        
+    }
+    
+    public int sumRange(int left, int right) {
+        if(left == 0) {
+            return prefix[right];
+        }
+        else {
+            return prefix[right] - prefix[left-1];
+        }
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
+
+ /* https://chatgpt.com/s/t_6ab2248fd48c8191ba6cb12274690565 */
